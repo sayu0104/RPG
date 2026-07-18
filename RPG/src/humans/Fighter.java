74442d95090ab96b3129eeaa4c63d056ec856cfd
yuzuko.saito <294679403+sayu0104@users.noreply.github.com>
2026-07-18 19:29:51 +0900

    ステップ３　Fighter.java　タスク①　完了

--- RPG/src/humans/Fighter.java
+++ RPG/src/humans/Fighter.java
@@ -1,7 +1,17 @@
 package humans;
 
 import bases.Human;
+import utils.Dice;
 
+// 戦士を表現する具象クラス
 public class Fighter extends Human {
 
+    // コンストラクタ
+    public Fighter(String name, String weapon) {
+    	super(name, weapon);// 【タスク①】親クラス（Human）のコンストラクタを利用して名前と武器をセットする
+        
+    	this.hp = Dice.get(240, 300);// 【タスク②】ヒットポイント（hp）の値を 240 〜 300 の乱数で決定してセットする
+        
+        // 【タスク③】攻撃力（offensive）の値を 17 〜 23 の乱数で決定してセットする
+    }
 }
