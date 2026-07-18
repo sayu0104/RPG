00f3230522ebf9ba62e61d0a74fa86f711e7e839
yuzuko.saito <294679403+sayu0104@users.noreply.github.com>
2026-07-18 19:22:02 +0900

    ステップ３　Brave.java　タスク③　完了

--- RPG/src/humans/Brave.java
+++ RPG/src/humans/Brave.java
@@ -12,6 +12,6 @@ public class Brave extends Human {
         
     	this.hp = Dice.get(170, 230);// 【タスク②】ヒットポイント（hp）の値を 170 〜 230 の乱数で決定してセットする
         
-        // 【タスク③】攻撃力（offensive）の値を 7 〜 13 の乱数で決定してセットする
+        this.offensive = Dice.get(7,13);// 【タスク③】攻撃力（offensive）の値を 7 〜 13 の乱数で決定してセットする
     }
 }
