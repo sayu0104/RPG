47b8f437979fc971c6e69bdf53ab65b54d832c34
yuzuko.saito <294679403+sayu0104@users.noreply.github.com>
2026-07-18 20:08:56 +0900

    ステップ３　slime.java　タスク③　完了

--- RPG/src/monsters/Slime.java
+++ RPG/src/monsters/Slime.java
@@ -10,8 +10,8 @@ public class Slime extends Monster {
     public Slime(String name, String weapon) {
     	super(name, weapon);// 【タスク①】親クラス（Monster）のコンストラクタを利用して名前と武器をセットする
         
-    	this.hp = Dice.get(270, 330);// 【タスク②】ヒットポイント（hp）の値を 70 〜 130 の乱数で決定してセットする
+    	this.hp = Dice.get(70, 130);// 【タスク②】ヒットポイント（hp）の値を 70 〜 130 の乱数で決定してセットする
         
-        // 【タスク③】攻撃力（offensive）の値を 5 〜 11 の乱数で決定してセットする
+    	this.offensive = Dice.get(5,11);// 【タスク③】攻撃力（offensive）の値を 5 〜 11 の乱数で決定してセットする
     }
 }
