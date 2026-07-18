0e9a681a53bef8f0b23b71f019a0b272c375ee87
yuzuko.saito <294679403+sayu0104@users.noreply.github.com>
2026-07-18 15:38:43 +0900

    ステップ３　タスク④　完了

--- RPG/src/bases/Monster.java
+++ RPG/src/bases/Monster.java
@@ -15,6 +15,6 @@ public abstract class Monster extends Living {
     public void attack(Living target) {
         int damage = offensive * Dice.get(1,10);// 【タスク②】1から10までのサイコロを振り、自分の攻撃力とかけ合わせた値を相手に与えるダメージとする
         target.setHp(target.getHp() - damage);// 【タスク③】相手のHPをダメージ値だけ減らす
-        // 【タスク④】コンソールにステータスを表示
+        System.out.println("「" + this.name + "」が「" + this.weapon + "」で攻撃！「" + target.getName() + "」に" + damage + "のダメージを与えた。");// 【タスク④】コンソールにステータスを表示
     }
 }
