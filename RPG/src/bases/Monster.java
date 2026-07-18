b410c9ce7c3281c1486462d7640a53ca86e4098c
yuzuko.saito <294679403+sayu0104@users.noreply.github.com>
2026-07-18 15:23:16 +0900

    ステップ３　タスク②　完了

--- RPG/src/bases/Monster.java
+++ RPG/src/bases/Monster.java
@@ -1,5 +1,20 @@
 package bases;
 
-public abstract class Monster {
+import utils.Dice;
 
+// モンスターを表現する抽象クラス
+public abstract class Monster extends Living {
+
+    // コンストラクタ
+    public Monster(String name, String weapon) {
+       super(name,weapon); // 【タスク①】Livingクラスで定義したコンストラクタを利用する
+    }
+
+    // attackメソッドのオーバーライド
+    @Override
+    public void attack(Living target) {
+        int damage = offensive * Dice.get(1,10);// 【タスク②】1から10までのサイコロを振り、自分の攻撃力とかけ合わせた値を相手に与えるダメージとする
+        // 【タスク③】相手のHPをダメージ値だけ減らす
+        // 【タスク④】コンソールにステータスを表示
+    }
 }
