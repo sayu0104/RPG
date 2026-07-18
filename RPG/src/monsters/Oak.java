8f046de0c33b3321b5ff1c4c378565bb0c7a12f1
yuzuko.saito <294679403+sayu0104@users.noreply.github.com>
2026-07-18 20:16:04 +0900

    ステップ３　Oak.java　タスク②　完了

--- RPG/src/monsters/Oak.java
+++ RPG/src/monsters/Oak.java
@@ -1,6 +1,7 @@
 package monsters;
 
 import bases.Monster;
+import utils.Dice;
 
 // オークを表現する具象クラス
 public class Oak extends Monster {
@@ -9,7 +10,7 @@ public class Oak extends Monster {
     public Oak(String name, String weapon) {
     	super(name, weapon);// 【タスク①】親クラス（Monster）のコンストラクタを利用して名前と武器をセットする
         
-        // 【タスク②】ヒットポイント（hp）の値を 170 〜 230 の乱数で決定してセットする
+    	this.hp = Dice.get(170, 230);// 【タスク②】ヒットポイント（hp）の値を 170 〜 230 の乱数で決定してセットする
         
         // 【タスク③】攻撃力（offensive）の値を 9 〜 15 の乱数で決定してセットする
     }
