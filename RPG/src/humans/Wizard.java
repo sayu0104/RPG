10761031dc4d35693cfe2c39eaf865b195bcde3a
yuzuko.saito <294679403+sayu0104@users.noreply.github.com>
2026-07-18 19:43:26 +0900

    ステップ３　Wizard.java　タスク①　完了

--- RPG/src/humans/Wizard.java
+++ RPG/src/humans/Wizard.java
@@ -2,6 +2,15 @@ package humans;
 
 import bases.Human;
 
+// 魔法使いを表現する具象クラス
 public class Wizard extends Human {
 
+    // コンストラクタ
+    public Wizard(String name, String weapon) {
+    	super(name, weapon);// 【タスク①】親クラス（Human）のコンストラクタを利用して名前と武器をセットする
+        
+        // 【タスク②】ヒットポイント（hp）の値を 120 〜 180 の乱数で決定してセットする
+        
+        // 【タスク③】攻撃力（offensive）の値を 12 〜 18 の乱数で決定してセットする
+    }
 }
