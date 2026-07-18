a485afb181da8f7c14a456d069765bea549951b5
yuzuko.saito <294679403+sayu0104@users.noreply.github.com>
2026-07-18 20:20:53 +0900

    ステップ３　Dragon.java　タスク②　完了

--- RPG/src/monsters/Dragon.java
+++ RPG/src/monsters/Dragon.java
@@ -1,6 +1,7 @@
 package monsters;
 
 import bases.Monster;
+import utils.Dice;
 
 // ドラゴンを表現する具象クラス
 public class Dragon extends Monster {
@@ -9,7 +10,7 @@ public class Dragon extends Monster {
     public Dragon(String name, String weapon) {
     	super(name, weapon);// 【タスク①】親クラス（Monster）のコンストラクタを利用して名前と武器をセットする
         
-        // 【タスク②】ヒットポイント（hp）の値を 270 〜 330 の乱数で決定してセットする
+    	this.hp = Dice.get(270, 330);// 【タスク②】ヒットポイント（hp）の値を 270 〜 330 の乱数で決定してセットする
         
         // 【タスク③】攻撃力（offensive）の値を 12 〜 18 の乱数で決定してセットする
     }
