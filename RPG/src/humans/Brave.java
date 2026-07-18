efd2a98bff1c3196c5e95f1f741ac220a54219b4
yuzuko.saito <294679403+sayu0104@users.noreply.github.com>
2026-07-18 19:14:03 +0900

    ステップ３　Brave.java　タスク②　完了

--- RPG/src/humans/Brave.java
+++ RPG/src/humans/Brave.java
@@ -1,15 +1,16 @@
 package humans;
 
 import bases.Human;
+import utils.Dice;
 
 // 勇者を表現する具象クラス
 public class Brave extends Human {
 
     // コンストラクタ
     public Brave(String name, String weapon) {
-       super(name,weapon); // 【タスク①】親クラス（Human）のコンストラクタを利用して名前と武器をセットする
+    	super(name, weapon);// 【タスク①】親クラス（Human）のコンストラクタを利用して名前と武器をセットする
         
-        // 【タスク②】ヒットポイント（hp）の値を 170 〜 230 の乱数で決定してセットする
+    	this.hp = Dice.get(170, 230);// 【タスク②】ヒットポイント（hp）の値を 170 〜 230 の乱数で決定してセットする
         
         // 【タスク③】攻撃力（offensive）の値を 7 〜 13 の乱数で決定してセットする
     }
